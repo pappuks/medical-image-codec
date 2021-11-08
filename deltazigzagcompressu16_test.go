fd6d8e6ec69e3fa483576438f56d09267db19fb1
Kuldeep Singh <pappuks@gmail.com>
2021-11-07 21:47:32 -0800

    Added more tests

--- deltazigzagcompressu16_test.go
+++ deltazigzagcompressu16_test.go
@@ -60,3 +60,27 @@ func TestDeltaZZRleCompression(t *testing.T) {
 		t.Errorf("Delta ZZ compression falied")
 	}
 }
+
+func TestDeltaZZRleCombinedCompression(t *testing.T) {
+	in := []uint16{256, 300, 468, 1025, 457, 399, 4096, 8000, 1, 65534, 0, 65535}
+	fmt.Println(in)
+	var comp DeltaRleZZU16
+	out, _ := comp.Compress(in, 4, 3, 65535)
+	fmt.Println(out)
+	var decomp DeltaRleZZU16
+	inAgain := decomp.Decompress(out, 4, 3)
+	fmt.Println(inAgain)
+	passed := true
+	for i, v := range in {
+		if v != inAgain[i] {
+			fmt.Printf("Value not same at %d original %d after %d\n", i, v, inAgain[i])
+			passed = false
+			break
+		}
+	}
+	if passed {
+		fmt.Printf("Delta ZZ Compression PASSED\n")
+	} else {
+		t.Errorf("Delta ZZ compression falied")
+	}
+}
